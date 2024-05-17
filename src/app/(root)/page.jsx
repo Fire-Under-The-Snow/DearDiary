3dfa0ca7ff95bace06403272284a77077cbc53cc
TenzDelek <tibetdelek@gmail.com>
2024-05-17 22:16:30 +0530

    ui inputed

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,7 +1,8 @@
+import { FlipWords } from "@/components/ui/flip-words";
 
 export default async function Home() {
   
- 
+  const words = ["Secure", "Community", "Healing", "Peace"];
   const maindata=[{
     name :"Tenzin Delek",
     message: " Lorem ipsum dolor sit amet, consectetur adipisicing elit."
@@ -34,8 +35,14 @@ export default async function Home() {
   
 ]
   return (
-    <div className=" bg-[#1C1C1C] ">
-        hello
+    <div className=" bg-[#1C1C1C] w-full h-delekheight flex justify-center items-center">
+        <div className="text-4xl mx-auto  text-neutral-400">
+        For
+        <FlipWords words={words} /> <br />
+        <div className=" text-lg">
+        Write with Dear Diary: A Project by Tenzin Delek
+          </div>
+      </div>
     </div>
 
   );
